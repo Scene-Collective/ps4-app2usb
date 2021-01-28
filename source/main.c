5ae090ef5024f112f1c9f65e1697f4e840c9a212
Al-Azif <alazif@miskatonic.io>
2021-01-27 17:47:49 -0800

    Change "socket" to "debug"

--- source/main.c
+++ source/main.c
@@ -752,7 +752,7 @@ int _main(struct thread *td) {
   nthread_run = 0;
 
 #ifdef DEBUG_SOCKET
-  printf_socket("\nClosing socket...\n\n");
+  printf_debug("Closing socket...\n");
   SckClose(DEBUG_SOCK);
 #endif
 
